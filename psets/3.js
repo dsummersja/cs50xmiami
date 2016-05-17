6d5d6936ca442a74c2b6e6cf608c1fb4e7365185
Dominic Summers <domz.summers@gmail.com>
2016-05-16 20:42:56 -0400

    pset3

--- psets/3.js
+++ psets/3.js
@@ -10,3 +10,8 @@ http://math2.org/math/geometry/circles.htm
 */
 
 // write your solution here...
+var radius = 7,
+    circum = 2 * 3.142 * radius,
+    area = 3.142 * radius * radius
+;
+console.log("The circumference is: "+circum+" and the Area is: "+area);
