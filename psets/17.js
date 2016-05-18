d6fbf188b50f383f1a6b9b84bb197aab30267036
Dominic Summers <domz.summers@gmail.com>
2016-05-17 21:37:31 -0400

    completed pset17

--- psets/17.js
+++ psets/17.js
@@ -10,3 +10,22 @@ Call that function for a few different scores and log the result to make sure it
 */
 
 // write your solution here...
+function pluralize(noun, number){
+  if (number == 1) {
+    console.log(number+" "+noun);
+  } else if (number > 1 && noun == "goose") {
+      console.log(number+" geese")
+  } else if (number > 1 && noun == "sheep") {
+      console.log(number+" "+noun)
+  } else if (number > 1 && noun == "fish") {
+      console.log(number+" "+noun)
+  } else if (number > 1) {
+      console.log(number+" "+noun+"s")
+  } else {
+    console.log("none")
+  }
+}
+pluralize("dog", 1);
+pluralize("goose", 2);
+pluralize("fish", 5);
+pluralize("sheep", 7);
