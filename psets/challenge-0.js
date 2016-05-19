aae4f040aeeb7e6ee9d269a76007b2d0c518c90e
Dominic Summers <domz.summers@gmail.com>
2016-05-18 20:25:52 -0400

    completed challenge-0.js

--- psets/challenge-0.js
+++ psets/challenge-0.js
@@ -1,23 +1,18 @@
 /*
 The Calculator
-
 - Write a function called squareNumber that will take one argument (a number),
   square that number, and return the result.
   It should also log a string like "The result of squaring the number 3 is 9."
-
 - Write a function called halfNumber that will take one argument (a number),
   divide it by 2, and return the result.
   It should also log a string like "Half of 5 is 2.5.".
-
 - Write a function called percentOf that will take two numbers,
   figure out what percent the first number represents of the second number,
   and return the result. It should also log a string like "2 is 50% of 4."
-
 - Write a function called areaOfCircle that will take one argument (the radius),
   calculate the area based on that, and return the result.
   It should also log a string like "The area for a circle with radius 2 is 12.566370614359172."
   - Bonus: Round the result so there are only two digits after the decimal.
-
 Write a function that will take one argument (a number) and perform the following operations,
   using the functions you wrote earlier:
   - Take half of the number and store the result.
@@ -26,5 +21,30 @@ Write a function that will take one argument (a number) and perform the followin
   - Calculate what percentage that area is of the squared result (#3).
 */
 
+function squareNumber(number){
+  return number*number;
+  //console.log("The result of squaring the number "+number+" is "+square);
+}
+function halfNumber(number){
+  return number/2;
 
-// write your solution here...
+  //console.log("Half of "+number+" is "+half);
+}
+function percentOf(num1, num2){
+  return (num1/num2)*100;
+  //console.log(num1+" is "+percentage+"% of "+num2);
+}
+function areaOfCircle(radius){
+  var area = 3.142 * (radius*radius);
+  console.log("The area for a circle with radius "+radius+" is "+area.toFixed(2));
+  return area.toFixed(2);
+}
+function calculate(number){
+  var ans1 = halfNumber(number),
+      ans2 = squareNumber(ans1),
+      ans3 = areaOfCircle(ans2),
+      ans4 = percentOf(ans3, ans2)
+  ;
+  console.log("Which is "+ans4+"% of the radius");
+}
+calculate(10);
